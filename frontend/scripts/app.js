bc14b4f4707caf7bd4c3fca145565ac33a4a35ee
unknown <hyllusx@gmail.com>
2025-12-02 16:34:50 +0200

    Included 'add transaction' feature

--- frontend/scripts/app.js
+++ frontend/scripts/app.js
@@ -30,3 +30,52 @@ new Chart(monthlyChart, {
         }]
     }
 })
+
+function createTransaction(){
+    let detailsDiv = document.querySelector('#details-div')
+    let trDate = document.getElementById('new-date').value
+    let trDesc = document.getElementById('new-desc').value
+    let trCat = document.getElementById('new-cat').value
+    let trType = document.getElementById('new-type').value
+    let trAmount = document.getElementById('new-amount').value
+
+    const newTransaction = document.createElement('div')
+    newTransaction.setAttribute('class', 'tr-details')
+    
+    let newDate = document.createElement('div')
+    newDate.setAttribute('class', 'date-div')
+    let dateText = document.createElement('p')
+    dateText.textContent = trDate
+    newDate.appendChild(dateText)
+    newTransaction.appendChild(newDate)
+
+    let newDesc = document.createElement('div')
+    newDesc.setAttribute('class', 'desc-div')
+    let descText = document.createElement('p')
+    descText.textContent = trDesc
+    newDesc.appendChild(descText)
+    newTransaction.appendChild(newDesc)
+
+    let newCat = document.createElement('div')
+    newCat.setAttribute('class', 'cat-div')
+    let catText = document.createElement('p')
+    catText.textContent = trCat
+    newCat.appendChild(catText)
+    newTransaction.appendChild(newCat)
+
+    let newType = document.createElement('div')
+    newType.setAttribute('class', 'type-div')
+    let typeText = document.createElement('p')
+    typeText.textContent = trType
+    newType.appendChild(typeText)
+    newTransaction.appendChild(newType)
+
+    let newAmount = document.createElement('div')
+    newAmount.setAttribute('class', 'amount-div')
+    let amountText = document.createElement('p')
+    amountText.textContent = trAmount
+    newAmount.appendChild(amountText)
+    newTransaction.appendChild(newAmount)
+
+    detailsDiv.appendChild(newTransaction)
+}
